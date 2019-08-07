08dc56866811620c30bb23f21e09b521dbdea842
kimj98 <52561488+kimj98@users.noreply.github.com>
2019-08-07 13:21:36 +0900

    Create constant.java

---
+++ CSBridge/src/main/java/com/fasoo/CSbridge/helper/constant.java
@@ -0,0 +1,22 @@
+package com.fasoo.CSbridge.helper;
+
+public class constant {
+    public static String Host = "192.168.100.30";
+    public static int Port = 9501;
+    public static String VirtualHost = "/wrapsody-oracle";
+    public static String Username = "wrapsody";
+    public static String Password = "wrapsody.1";
+    public static String KrFile = "../CSBridge/WrapsodyUserGuide.top";
+    public static String EnFile = "../CSBridge/WrapsodyUserGuide_en.top";
+    public static String QueueName = "bot-chatscript";
+    public static String MSRoutingkey = "#.user.@Bot@chatscript";
+    public static String MSProperty = "com.wrapsody.messaging.model.Message";
+    public static String LMPropoerty = "com.wrapsody.messaging.model.ApiResponse";
+    public static String MRoutingkey = "api.admin.mail.send";
+    public static String MProperty = "com.wrapsody.messaging.model.ApiRequest";
+
+
+
+
+
+}
