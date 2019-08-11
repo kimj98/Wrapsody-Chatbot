c99ea77f974fa9882f14319882a17eb83ab8f558
TaehoonHoodyKim <40323560+TaehoonHoodyKim@users.noreply.github.com>
2019-08-12 04:01:19 +0900

    Update constant.java

--- CSBridge/src/main/java/com/fasoo/CSbridge/helper/constant.java
+++ CSBridge/src/main/java/com/fasoo/CSbridge/helper/constant.java
@@ -2,24 +2,25 @@ package com.fasoo.CSbridge.helper;
 
 public class constant {
 
+    // 2019.8.12 : fix the info for security
     //[ RabbitmQ Connection ]
-    public static final String Host = "192.168.100.30";
-    public static final int Port = 9501;
-    public static final String VirtualHost = "/wrapsody-oracle";
-    public static final String Username = "wrapsody";
-    public static final String Password = "Wrapsody.1";
+    public static final String Host = "xxx.xxx.xx.xx";
+    public static final int Port = 0001;
+    public static final String VirtualHost = "/Virtual-HostName";
+    public static final String Username = "DummyName";
+    public static final String Password = "DummyPW";
 
     //[ Comment Reader.java part ]
-    public static final String KrFile = "../CSBridge/WrapsodyUserGuide.top";
-    public static final String EnFile = "../CSBridge/WrapsodyUserGuide_en.top";
+    public static final String KrFile = "../CSBridge/KoreanVersion.top";
+    public static final String EnFile = "../CSBridge/EngVersion.top";
 
     //[MsgCatch.java  & MsgSend.java part ]
-    public static final String QueueName = "bot-chatscript";
+    public static final String QueueName = "testQueueName";
     public static final String MSRoutingkey = "#.user.@Bot@chatscript";
-    public static final String MSProperty = "com.wrapsody.messaging.model.Message";
+    public static final String MSProperty = "PropertyNameHere";
 
     //[Left Menu Send Part ]
-    public static final String LMenuPropoerty = "com.wrapsody.messaging.model.ApiResponse";
-    public static final String MailRoutingKey = "api.admin.mail.send";
-    public static final String MailProperty = "com.wrapsody.messaging.model.ApiRequest";
+    public static final String LMenuPropoerty = "Property_Name_Here";
+    public static final String MailRoutingKey = "Api_Routing_Key";
+    public static final String MailProperty = "Api_Routing_Key";
 }
